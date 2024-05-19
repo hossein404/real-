6599ace43f6f306e85a1be748dffe7ac2c1756bc
mtashani <tahani@outlook.com>
2024-05-20 01:26:32 +0430

    fix timing

--- realUpload.py
+++ realUpload.py
@@ -1,9 +1,7 @@
 import os
 import random
 import requests
-import shutil
 import time
-import schedule
 from datetime import datetime
 
 def download_file(url, destination, max_speed=102400, timeout=180):
@@ -42,39 +40,34 @@ def download_file(url, destination, max_speed=102400, timeout=180):
 
 
 def main():
-    current_time = datetime.now().time()
-    start_time = datetime.strptime("23:00:00", "%H:%M:%S").time()
-    end_time = datetime.strptime("08:00:00", "%H:%M:%S").time()
-    
-    # if current_time >= start_time or current_time <= end_time:
-    file_path = "urls.txt"
-    with open(file_path, "r") as file:
-        urls = file.readlines()
-    random_url = random.choice(urls).strip()  # Randomly select one URL from the list
-    file_name = random_url.split("/")[-1]
-    
-    if current_time >= datetime.strptime("01:00:00", "%H:%M:%S").time() and current_time <= datetime.strptime("07:30:00", "%H:%M:%S").time():
-        max_speed = 24000 * 1024  
-    else:
-        max_speed = 12000 * 1024  
-
-    print(f"Downloading file from: {random_url}")
-    download_file(random_url, file_name, max_speed=max_speed, timeout=180)  # Max speed in bytes per second, timeout in seconds
-    try:
-        os.remove(file_name)  # Remove the downloaded file
-        print(f"File {file_name} removed!\n")
-    except FileNotFoundError:
-        print(f"File {file_name} not found!\n")
-    # else:
-    #     print("Current time is outside the allowed range (11 PM to 8 AM). Task will not run.\n")
-
-def schedule_task():
-    schedule.every(3).to(5).minutes.do(main)
-
     while True:
-        schedule.run_pending()
-        time.sleep(1)
+        current_time = datetime.now().time()
+        start_time = datetime.strptime("23:00:00", "%H:%M:%S").time()
+        end_time = datetime.strptime("08:00:00", "%H:%M:%S").time()
+        
+        # if current_time >= start_time or current_time <= end_time:
+        file_path = "urls.txt"
+        with open(file_path, "r") as file:
+            urls = file.readlines()
+        random_url = random.choice(urls).strip()  # Randomly select one URL from the list
+        file_name = random_url.split("/")[-1]
+        
+        if current_time >= datetime.strptime("01:00:00", "%H:%M:%S").time() and current_time <= datetime.strptime("07:30:00", "%H:%M:%S").time():
+            max_speed = 24000 * 1024  
+        else:
+            max_speed = 12000 * 1024  
+
+        print(f"Downloading file from: {random_url}")
+        random_timeout = random.randint(180, 300)
+        download_file(random_url, file_name, max_speed=max_speed, timeout=random_timeout)  # Max speed in bytes per second, timeout in seconds
+        try:
+            os.remove(file_name)  # Remove the downloaded file
+            print(f"File {file_name} removed!\n")
+        except FileNotFoundError:
+            print(f"File {file_name} not found!\n")
+        random_sleep = random.randint(10, 120)
+        time.sleep(random_sleep)
 
 if __name__ == "__main__":
-    schedule_task()
+    main()
 
